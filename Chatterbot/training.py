83d3d5e620e45aa6420c98247226a1621d281b56
Shivam Goyal <shivam1111@gmail.com>
2016-02-01 14:10:24 +0530

    Added chatter bot examples

---
+++ Chatterbot/training.py
@@ -0,0 +1,27 @@
+from chatterbot import ChatBot
+chatbot = ChatBot("Ron Obvious")
+
+# Train based on the english corpus
+chatbot.train("chatterbot.corpus.english")
+
+# Train based on english greetings corpus
+chatbot.train("chatterbot.corpus.english.greetings")
+
+# Train based on the english conversations corpus
+chatbot.train("chatterbot.corpus.english.conversations")
+
+conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome."
+]
+
+chatbot.train(conversation)
+
+
+# Get a response to an input statement
+chatbot.get_response("How are you?")
